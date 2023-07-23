725defb02de219c4fd8e9583b78e15efcfdd23d2
vladydev3 <vladypv3@gmail.com>
2023-07-23 19:43:56 -0400

    borrar archivos auxiliares

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -9,7 +9,7 @@ show_report(){
     if [ ! -f "../informe/inf.pdf" ]; then
         pdflatex "../informe/inf.tex"
         echo "Creando inf.pdf..."
-        xdg-open ../informe/inf.pdf
+        xdg-usen ../informe/inf.pdf
         echo "Mostrando inf.pdf..."
     fi
 }
@@ -17,7 +17,7 @@ show_report(){
 show_slide(){
     if [ ! -f "../presentacion/pres.pdf"];then
         pdflatex "../presentacion/pres.tex"
-        xdg-open ../presentacion/pres.pdf
+        xdg-usen ../presentacion/pres.pdf
         echo "Mostrando pres.tex"
     fi
 }
@@ -32,54 +32,46 @@ create_slide(){
     echo "Archivo pres.pdf creado con éxito."
 }
 
-#clean_files()
-#    
-#    cd ../informe
-#    rm -f "*.aux" "*.lot" "*.lof" "*.log" "*.out" "*.gz" "*.toc" "*.fls" "*.fdb_latexmk"
-#
-#    cd ./../presentacion
-#    rm -f "slide.aux" "slide.log" "slide.out" "slide.synctex.gz" "slide.toc" "slide.fls" "slide.fdb_latexmk" "slide.vrb" "slide.nav" "slide.snm"
-#
-#    echo "Archivos auxiliares borrados"
+clean_files(){   
+   cd ../informe
+   rm -f "*.aux" "*.lot" "*.lof" "*.log" "*.out" "*.gz" "*.toc" "*.fls" "*.fdb_latexmk"
+
+   cd ../presentacion
+   rm -f "*.aux" "*.log" "*.out" "*.synctex.gz" "*.toc" "*.fls" "*.fdb_latexmk" "*.vrb" "*.nav" "*.snm"
+
+   echo "Archivos auxiliares borrados"
+}
 
 while  true 
 do
     echo -e "Escriba un numero dependiendo de la opcion que desee: \n1-> run \n2-> report \n3-> slides \n4-> show_report \n5-> show_slides \n6-> clean \n7-> exit" 
-    read op
+    read us
 
-    if [[ ! $op =~ ^[0-9]+$ ]]
-    then
+    if [[ ! $us =~ ^[0-9]+$ ]];then
     echo -e "\n"
-    elif [ $op -eq 1 ] 
-    then 
+
+    elif [ $us -eq 1 ];then 
     run_moogle
 
-    elif [ $op -eq 2 ]
-    then
+    elif [ $us -eq 2 ];then
     create_report
 
-    elif [ $op -eq 3 ]
-    then
+    elif [ $us -eq 3 ];then
     create_slide
 
-    elif [ $op -eq 4 ]
-    then
+    elif [ $us -eq 4 ];then
     show_report
 
-    elif [ $op -eq 5 ]
-    then
+    elif [ $us -eq 5 ];then
     show_slide
   
+    elif [ $us -eq 6 ];then
+    clean_files
 
-    elif [ $op -eq 6 ]
-    then
-    echo "nada"
-
-    elif [ $op -eq 7 ]
-    then
+    elif [ $us -eq 7 ];then
     exit
 
     else
-    echo -e "You did not select any of the given numbers \n"
+    echo -e "Seleccione un numero valido \n"
     fi
 done
